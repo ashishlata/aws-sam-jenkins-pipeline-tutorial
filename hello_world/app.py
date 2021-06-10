995ab9d42a0f819280fecdc2369ddebac385efa3
ashishlata <ashishlata1995@gmail.com>
2021-06-10 02:28:36 -0500

    changed-for-code-deploy

--- hello_world/app.py
+++ hello_world/app.py
@@ -4,7 +4,7 @@ import json
 
 
 def lambda_handler(event, context):
-    print('this is  service two deployement')
+    print('this is sample python file')
     """Sample pure Lambda function
 
     Parameters
